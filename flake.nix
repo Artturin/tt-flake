b8da300c94c046095f1d26907412bb278b4ef6c4
Artturin <Artturin@artturin.com>
2024-10-01 23:56:48 +0300

    Add flake-compat files

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs =
