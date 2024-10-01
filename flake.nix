3479ca9d8366a19839867e675e521c6dfcfc3aa6
Artturin <Artturin@artturin.com>
2024-10-01 21:45:13 +0300

    Use `eachDefaultSystem`

--- flake.nix
+++ flake.nix
@@ -3,46 +3,52 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
+    inputs:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      kernel = pkgs.linux_latest;
-      kmd = import ./pkgs/kmd { inherit pkgs kernel; };
-      sfpi = import ./pkgs/sfpi { inherit pkgs; };
-      luwen = import ./pkgs/luwen { inherit pkgs; };
-      common = import ./pkgs/common { inherit pkgs; };
-      flash = import ./pkgs/flash {
-        inherit pkgs;
-        pyluwen = luwen.pyluwen_0_1;
-      };
-      smi = import ./pkgs/smi {
-        inherit pkgs;
-        pyluwen = luwen.pyluwen;
-        tools-common = common;
-      };
-      umd = import ./pkgs/umd { inherit pkgs; };
+      inherit (inputs) self;
+      inherit (inputs.nixpkgs) lib;
     in
-    {
-      packages.${system} = {
-        kmd = kmd.kmd;
-        udev-rules = kmd.udev-rules;
-        kmd-test = kmd.test;
-        sfpi = sfpi.sfpi;
-        tt-gcc = sfpi.tt-gcc;
-        smi = smi;
-        luwen = luwen.luwen;
-        pyluwen = luwen.pyluwen;
-        tools-common = common;
-        flash = flash;
-        umd = umd;
-      };
+    inputs.utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        kernel = pkgs.linux_latest;
+        kmd = import ./pkgs/kmd { inherit pkgs kernel; };
+        sfpi = import ./pkgs/sfpi { inherit pkgs; };
+        luwen = import ./pkgs/luwen { inherit pkgs; };
+        common = import ./pkgs/common { inherit pkgs; };
+        flash = import ./pkgs/flash {
+          inherit pkgs;
+          pyluwen = luwen.pyluwen_0_1;
+        };
+        smi = import ./pkgs/smi {
+          inherit pkgs;
+          pyluwen = luwen.pyluwen;
+          tools-common = common;
+        };
+        umd = import ./pkgs/umd { inherit pkgs; };
+      in
+      {
+        packages = {
+          kmd = kmd.kmd;
+          udev-rules = kmd.udev-rules;
+          kmd-test = kmd.test;
+          sfpi = sfpi.sfpi;
+          tt-gcc = sfpi.tt-gcc;
+          smi = smi;
+          luwen = luwen.luwen;
+          pyluwen = luwen.pyluwen;
+          tools-common = common;
+          flash = flash;
+          umd = umd;
+          default = self.packages.${system}.smi;
+        };
 
-      formatter.${system} = pkgs.nixfmt-rfc-style;
-
-      defaultPackage.${system} = self.packages.${system}.smi;
-    };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
